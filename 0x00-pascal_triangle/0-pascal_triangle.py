3a5cdba065ce41ea6062178d9271e0d7c39ed958
root <jedidiabahena@gmail.com>
2022-12-22 13:52:10 +0000

    added answer file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Pascal triangle module
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of intergers representing the Pascal's
+    triangle of n
+    """
+    if type(n) is int:
+        triangle = []
+        if n <= 0:
+            return triangle
+        else:
+            prev = [1]
+            for row in range(n):
+                a_list = []
+                if row == 0:
+                    a_list = [1]
+                else:
+                    for i in range(row + 1):
+                        if i == 0:
+                            a_list.append(prev[i])
+                        elif i == (row):
+                            a_list.append(prev[i - 1])
+                        else:
+                            a_list.append(prev[i - 1] + prev[i])
+                prev = a_list
+                triangle.append(a_list)
+            return triangle
+                        
